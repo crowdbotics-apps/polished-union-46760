8d476a03167f9780e36dbdad4f970e7ef403b9fd
Crowdbotics <team@crowdbotics.com>
2024-03-01 20:22:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-union-46760",
+  url: "https://polished-union-46760.botics.co",
+  api: "https://polished-union-46760.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
